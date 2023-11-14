9cb1b71f0b4c4609433ab7a14d6d31f62927d88f
CorrectRoadH <a778917369@gmail.com>
2023-11-14 18:25:05 +0800

    feat: update json editor

--- components/json-editor/index.tsx
+++ components/json-editor/index.tsx
@@ -3,6 +3,13 @@ import { useDebouncedEffect } from '@react-hookz/web';
 import { useState } from 'react';
 import { toast } from 'sonner';
 
+import { render } from "react-dom";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+
 interface JsonEditorProps {
     json:string
     setJson:(json:string)=>void
@@ -35,11 +42,20 @@ const JsonEditor =({json,setJson}:JsonEditorProps)=>{
     return (
         <div className="w-5/12 rounded-3xl p-5">
           <div className="text-3xl	font-black">XState Json</div>
-          <textarea 
-            className="textarea textarea-primary w-full h-[45rem]" 
-            value={json}
-            onChange={(e) => setJson(e.target.value)}
-          />
+            {/* <textarea 
+              className="textarea textarea-primary w-full h-[45rem]" 
+              value={json}
+              onChange={(e) => setJson(e.target.value)}
+            /> */}
+             <AceEditor
+              className="textarea textarea-primary !w-full !h-[45rem]" 
+              mode="json"
+              theme="github"
+              onChange={setJson}
+              value={json}
+              name="UNIQUE_ID_OF_DIV"
+              editorProps={{ $blockScrolling: true }}
+            />
 
             <div className="flex py-2 gap-5">
                 <button className="btn btn-neutral"
